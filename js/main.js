8c627dbaa5bc9faa5f352b7b069b8514325f1fe4
Alexandra Ralev <alexandrapredaralev@gmail.com>
2018-05-24 11:46:14 +0100

    add basic click logic

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-main.js
+let cat = document.getElementById('cat'), 
+	counterText = document.getElementById('counter'), 
+	counter = 0;
+
+cat.addEventListener('click', () => {
+	console.log('click');
+	counter += 1;
+	counterText.innerText = counter; 
+
+});
