7e543a70972f287942e5d6e99347b5c8811df479
Alexandra Ralev <alexandrapredaralev@gmail.com>
2018-05-24 12:53:34 +0100

    fix logic for the second cat

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
-let cat = document.getElementById('cat'), 
-	counterText = document.getElementById('counter-text'), 
-	counter = 0;
+const cats = Array.from(document.querySelectorAll('.cat')), 
+	counterText = document.getElementById('counter-text'); 
+let	counter = 0;
 
-cat.addEventListener('mousedown', function() {
-	counter += 1;
-	counterText.innerText = counter; 
-	cat.setAttribute('src', 'img/cat-click.svg');
+
+
+function flash(e) {
+	e.target.setAttribute('src', 'img/cat.svg');
+}
+
+cats.forEach(function(cat, index){
+	cat.addEventListener('mousedown', function(e) {
+		counter += 1;
+		counterText.innerText = counter; 
+		e.target.setAttribute('src', `img/cat${index}-click.svg`);
+	});
+	cat.addEventListener('mouseup', function(e) {
+		e.target.setAttribute('src', `img/cat${index}.svg`);
+	});
 });
 
-cat.addEventListener('mouseup', function() {
-	cat.setAttribute('src', 'img/cat.svg');
-});
 
 
+//Array.from('cat', c => c.addEventListener('mouseup', increment));
