e3a1ecff08f5cc9125ef05fcdf3f63d40c3b12c7
Alexandra Ralev <alexandrapredaralev@gmail.com>
2018-05-24 11:55:11 +0100

    FIX linking error

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 let cat = document.getElementById('cat'), 
-	counterText = document.getElementById('counter'), 
+	counterText = document.getElementById('counter-text'), 
 	counter = 0;
 
-cat.addEventListener('click', () => {
-	console.log('click');
+cat.addEventListener('mousedown', function() {
 	counter += 1;
 	counterText.innerText = counter; 
-
+	cat.setAttribute('src', 'img/cat-click.svg');
 });
+
+cat.addEventListener('mouseup', function() {
+	cat.setAttribute('src', 'img/cat.svg');
+});
+
+
