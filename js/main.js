d7412f39ddef3306a0a44896055d2aa3b11eeb8f
Alexandra Ralev <alexandrapredaralev@gmail.com>
2018-05-24 17:37:40 +0100

    FIX dragging

--- js/main.js
+++ js/main.js
@@ -2,15 +2,20 @@ const 	cats = Array.from(document.querySelectorAll('.cat')),
 counters = Array.from(document.querySelectorAll('.counter')); 
 
 cats.forEach(function(cat, index){
+	//start a closed counter for each cat at 0
 	cat.addEventListener('mousedown', (function(count) {
 			return function(e) {
 				count +=1;
 				counters[index].innerText = count;
+				e.preventDefault();
 				e.target.setAttribute('src', `img/cat${index}-click.svg`);
 			};
 	})(0));
 	
 	cat.addEventListener('mouseup', function(e) {
+		e.target.setAttribute('src', `img/cat${index}.svg`);
+	});
+	cat.addEventListener('mouseout', function(e) {
 		e.target.setAttribute('src', `img/cat${index}.svg`);
 	});
 });
