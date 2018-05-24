944fe2cebb62e2f62a7c095ff116c4a0fe4481c4
Alexandra Ralev <alexandrapredaralev@gmail.com>
2018-05-24 17:32:25 +0100

    refactor using closures

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 const 	cats = Array.from(document.querySelectorAll('.cat')), 
 counters = Array.from(document.querySelectorAll('.counter')); 
-let n = 0; //this little variable will temporarily stopre the inside of the cat clicker counter, inclrease by 1 on click and then become the inner text of the counter
 
 cats.forEach(function(cat, index){
-	cat.addEventListener('mousedown', function(e) {
-		n = Number(counters[index].innerText) + 1;
-		counters[index].innerText = n;
-		e.target.setAttribute('src', `img/cat${index}-click.svg`);
-	});
+	cat.addEventListener('mousedown', (function(count) {
+			return function(e) {
+				count +=1;
+				counters[index].innerText = count;
+				e.target.setAttribute('src', `img/cat${index}-click.svg`);
+			};
+	})(0));
+	
 	cat.addEventListener('mouseup', function(e) {
 		e.target.setAttribute('src', `img/cat${index}.svg`);
 	});
 });
-
-
-
